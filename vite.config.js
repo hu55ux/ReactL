6a87ae71d97fc10679314fb2c4623927406432db
Huseyn <hsbziyev@gmail.com>
2025-12-11 11:24:56 +0400

    HH

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,4 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
-  build: {
-    copyPublicDir: false,
-  },
 })
