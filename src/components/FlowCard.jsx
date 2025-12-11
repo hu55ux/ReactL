5d510fe23e48c526bced328ebed71707c61c5855
Huseyn <hsbziyev@gmail.com>
2025-12-11 11:11:30 +0400

    S

--- src/components/FlowCard.jsx
+++ src/components/FlowCard.jsx
@@ -9,10 +9,10 @@ export function FlowCard() {
             imgSrc="https://flowbite-react.com/images/blog/image-1.jpg"
         >
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
+                Noteworthy technology acquisitions 20211
             </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                Here are the biggest enterprise technolodgy acquisitions of 2021 so far, in reverse chronological order.
             </p>
         </Card>
     );
